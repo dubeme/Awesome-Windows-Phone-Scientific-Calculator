6ef332e604f1bb126b1ad5586756678e72ac24f6
Dubem Enyekwe <dubem@dubemenyekwe.com>
2013-07-24 00:25:17 -0700

    Added the pythagorean theorem class

---
+++ EquationLibrary/Equation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace EquationLibrary
+{
+    public class Equation
+    {
+        public double Result { get; set; }
+        public string Error { get; set; }
+    }
+}
---
+++ EquationLibrary/Trigonometry/PythagoreanTheorem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EquationLibrary.Trigonometry
+{
+    public class PythagoreanTheorem : Equation
+    {
+        public double Hypothenus { get; set; }
+        public double Opposite { get; set; }
+        public double Adjacent { get; set; }
+
+        /// <summary>
+        /// Solve for the Hypothenus side of the right angle triangle.
+        /// </summary>
+        /// <returns>True if solved, else false.</returns>
+        public bool solveHypothenus()
+        {
+            try
+            {
+                Hypothenus = Math.Sqrt(Math.Pow(Opposite, 2.0) + Math.Pow(Adjacent, 2.0));
+                return true;
+            }
+            catch (Exception) { }
+            return false;
+        }
+
+        /// <summary>
+        /// Solve for the Opposite side of the right angle triangle.
+        /// </summary>
+        /// <returns>True if solved, else false.</returns>
+        public bool solveOpposite()
+        {
+            try
+            {
+                Opposite = Math.Sqrt(Math.Pow(Hypothenus, 2.0) - Math.Pow(Adjacent, 2.0));
+                return true;
+            }
+            catch (Exception) { }
+            return false;
+        }
+
+        /// <summary>
+        /// Solve for the Adjacent side of the right angle triangle.
+        /// </summary>
+        /// <returns>True if solved, else false.</returns>
+        public bool solveAdjacent()
+        {
+            try
+            {
+                Adjacent = Math.Sqrt(Math.Pow(Hypothenus, 2.0) - Math.Pow(Opposite, 2.0));
+                return true;
+            }
+            catch (Exception) { }
+            return false;
+        }
+    }
+}
